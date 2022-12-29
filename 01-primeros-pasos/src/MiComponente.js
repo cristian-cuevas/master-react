039683bd32df1db608dbd22fc202f906433c5b01
cristian-cuevas <cuevasgarciascristian@gmail.com>
2022-12-29 16:25:22 +0000

    master

---
+++ 01-primeros-pasos/src/MiComponente.js
@@ -0,0 +1,16 @@
+//importar modulos de react/dependencias
+import React,{Fragment} from "react";
+
+
+//funcion del componente 
+const MiComponente = () => {
+    Fragment
+
+    return( <p> es es mi primer componente</p>
+    
+    
+    );
+};
+
+// export 
+export default MiComponente;
