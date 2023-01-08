b2770217f112c0d193dc1dd0cfd02011545a3d60
cristian-cuevas <cuevasgarciascristian@gmail.com>
2023-01-08 15:17:59 +0000

    avanzando

--- 03-use-effect/src/componnents/AjaxComponent.js
+++ 03-use-effect/src/componnents/AjaxComponent.js
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from 'react'
 export const AjaxComponent = () => {
 
 const [usuarios, setUsuarios] = useState([]);
+const [cargando, setCargando] = useState(true);
 
 //Generico o basico 
 
@@ -34,11 +35,51 @@ const [usuarios, setUsuarios] = useState([]);
 
         ]);
     }
-    useEffect(() => {
-        getUsuariosEstaticos();
-    }, []);
 
-   
+    const getUsuariosAjaxPms = () => {
+        fetch("https://reqres.in/api/users?page=1")
+        .then(respuesta => respuesta.json() )
+        .then(resultado_final => { 
+            setUsuarios(resultado_final.data)
+            console.log(usuarios)
+        },
+        error => {
+            console.log(error);
+        }
+        )
+    }
+
+    const getUsuariosAjaxAW = () => {
+
+        setTimeout(async() => {
+            const peticion = await fetch("https://reqres.in/api/users?page=1");
+        const {data} =  peticion.json;
+
+        setUsuarios(data);
+        setCargando(false);
+            alert("Hola")
+        },2000);
+
+        
+    }
+
+
+    useEffect(() => {
+       // getUsuariosEstaticos();
+       getUsuariosAjaxPms();
+
+       //getUsuariosAjaxAW();
+    }, []);
+    
+    if ( cargando == true){
+        //cuando esta todo cargando
+return (
+    <div className='cargando'>
+        Cargando datos ...
+    </div>
+);
+    } else{
+        //cuando todo ha ido bien 
     return (
         <div>
             <h2>Listado de usuarios via Ajax </h2>
@@ -46,10 +87,18 @@ const [usuarios, setUsuarios] = useState([]);
                 {
                     usuarios.map(usuario => {
                         console.log(usuario);
-                        return <li key={usuario.id}>{usuario.first_name} {usuario.last_name}</li>
+                        return <li key={usuario.id}>
+                            < img src={usuario.avatar} width="20"/>
+                            &nbsp;
+                            {usuario.first_name} {usuario.last_name}</li>
                     })
                 }
             </ol>
         </div>
     )
 }
+
+    }
+    
+
+    
